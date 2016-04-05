1fa6ecd26acdafb065897438e8f5d5bc783b151f
Alex Gusev <alex@flancer64.com>
2016-04-05 20:29:38 +0300

    MOBI-265 - unit tests (transaction manager)

---
+++ test/unit/Repo/Def/TransactionDefinition_Test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Core\Repo\Def;
+
+
+include_once(__DIR__ . '/../../phpunit_bootstrap.php');
+
+class TransactionDefinition_UnitTest extends \Praxigento\Core\Lib\Test\BaseMockeryCase
+{
+    /** @var  TransactionDefinition */
+    private $obj;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        /* create mocks */
+        /* create object */
+        $this->obj = new TransactionDefinition();
+    }
+
+
+    public function test_accessors()
+    {
+        $LEVEL = 21;
+        $this->obj->setLevel($LEVEL);
+        $this->assertEquals($LEVEL, $this->obj->getLevel());
+    }
+
+}
---
+++ test/unit/Repo/Def/TransactionManager_Test.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Core\Repo\Def;
+
+
+include_once(__DIR__ . '/../../phpunit_bootstrap.php');
+
+class TransactionManager_UnitTest extends \Praxigento\Core\Lib\Test\BaseMockeryCase
+{
+    /** @var  \Mockery\MockInterface */
+    private $mConn;
+    /** @var  \Mockery\MockInterface */
+    private $mManObj;
+    /** @var  TransactionManager */
+    private $obj;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        /* create mocks */
+        $this->mConn = $this->_mockConn();
+        $this->mManObj = $this->_mock(\Magento\Framework\ObjectManagerInterface::class);
+        /* create object */
+        $mResource = $this->_mockResourceConnection($this->mConn);
+        $this->obj = new TransactionManager($mResource, $this->mManObj);
+    }
+
+    public function test_begin()
+    {
+        /* === Test Data === */
+        $LEVEL = 1;
+        /* === Setup Mocks === */
+        // $this->_conn->beginTransaction();
+        $this->mConn
+            ->shouldReceive('beginTransaction')->once();
+        // $result = $this->manObj->create(ITransactionDefinition::class);
+        $mTrans = new TransactionDefinition();
+        $this->mManObj
+            ->shouldReceive('create')->once()
+            ->andReturn($mTrans);
+        // $result->setLevel($this->_transactionLevel);
+        /* === Call and asserts  === */
+        $res = $this->obj->transactionBegin();
+        $this->assertEquals($LEVEL, $res->getLevel());
+    }
+
+    public function test_close_committed()
+    {
+        /* === Test Data === */
+        $LEVEL = 1;
+        $mTrans = new TransactionDefinition();
+        $mTrans->setLevel($LEVEL);
+        /* === Setup Mocks === */
+        // $this->_conn->rollBack();
+        $this->mConn
+            ->shouldReceive('rollBack')->once();
+        /* === Call and asserts  === */
+        $this->obj->transactionClose($mTrans);
+    }
+    public function test_close_other()
+    {
+        /* === Test Data === */
+        $LEVEL = 2;
+        $mTrans = new TransactionDefinition();
+        $mTrans->setLevel($LEVEL);
+        /* === Setup Mocks === */
+        $this->mConn
+            ->shouldNotReceive('rollBack');
+        /* === Call and asserts  === */
+        $this->obj->transactionClose($mTrans);
+    }
+
+    public function test_close_rollback()
+    {
+        /* === Test Data === */
+        $LEVEL = 0;
+        $mTrans = new TransactionDefinition();
+        $mTrans->setLevel($LEVEL);
+        /* === Setup Mocks === */
+        // $this->_conn->rollBack();
+        $this->mConn
+            ->shouldReceive('rollBack')->once();
+        /* === Call and asserts  === */
+        $this->obj->transactionClose($mTrans);
+    }
+
+    public function test_commit()
+    {
+        /* === Test Data === */
+        $LEVEL = 0;
+        $mTrans = new TransactionDefinition();
+        $mTrans->setLevel($LEVEL);
+        /* === Setup Mocks === */
+        // $this->_conn->commit();
+        $this->mConn
+            ->shouldReceive('commit')->once();
+        /* === Call and asserts  === */
+        $this->obj->transactionCommit($mTrans);
+    }
+
+    public function test_rollback()
+    {
+        /* === Test Data === */
+        $LEVEL = 0;
+        $mTrans = new TransactionDefinition();
+        $mTrans->setLevel($LEVEL);
+        /* === Setup Mocks === */
+        // $this->_conn->rollBack();
+        $this->mConn
+            ->shouldReceive('rollBack')->once();
+        /* === Call and asserts  === */
+        $this->obj->transactionRollback($mTrans);
+    }
+
+}
