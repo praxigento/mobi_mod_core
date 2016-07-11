a253544b00dc787dddb66d879048cdb5bb3c7da8
Alex Gusev <alex@flancer64.com>
2016-07-11 11:40:19 +0300

    MOBI-330 - Fix tests (unit & integration)

---
+++ test/integration/Setup/Main_Test.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Core\Lib\Test\Setup;
+
+include_once(__DIR__ . '/../phpunit_bootstrap.php');
+
+class Main_IntegrationTest
+    extends \Praxigento\Core\Test\BaseIntegrationTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->_logger = $this->_manObj->get('loggerSetup'); // use standalone logger for setup logs
+    }
+
+
+    public function test_main()
+    {
+        $this->_logger->debug('Setup Integration tests is started.');
+        $this->_conn->beginTransaction();
+        try {
+            $tbls = $this->_conn->getTables();
+            $this->_logger->info("All tables:\n" . var_export($tbls, true));
+        } finally {
+            // $this->_conn->commit();
+            $this->_conn->rollBack();
+        }
+        $this->_logger->debug('Setup Integration test is completed, all transactions are rolled back.');
+    }
+}
