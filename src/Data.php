d699a87640c22b8297dd2c5acd7036fc4bc4cdad
Alex Gusev <alex@flancer64.com>
2017-08-24 16:55:23 +0300

    MOBI-867 - Refactor period service

---
+++ src/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Core;
+
+/**
+ * Wrapper for external library's object.
+ */
+class Data
+    extends \Flancer32\Lib\Data
+{
+
+}
