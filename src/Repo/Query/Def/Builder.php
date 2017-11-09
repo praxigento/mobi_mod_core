ac37a21b536cf1a7279c37c616442fbb754eab99
Alex Gusev <alex@flancer64.com>
2017-11-09 13:39:02 +0200

    MOBI-946 Remove deprecated methods from \Praxigento\Downline\Repo\Query\Snap\OnDate\Builder

--- src/Repo/Query/Def/Builder.php
+++ src/Repo/Query/Def/Builder.php
@@ -2,6 +2,7 @@
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
+
 namespace Praxigento\Core\Repo\Query\Def;
 
 /**
@@ -10,7 +11,7 @@ namespace Praxigento\Core\Repo\Query\Def;
  * @deprecated use \Praxigento\Core\Repo\Query\Builder
  */
 abstract class Builder
-    implements \Praxigento\Core\Repo\Query\IBuilder, \Praxigento\Core\Repo\Query\IBuilder2
+    implements \Praxigento\Core\Repo\Query\IBuilder2
 {
     /** @var  \Magento\Framework\DB\Adapter\AdapterInterface */
     protected $conn; // default connection
@@ -20,7 +21,8 @@ abstract class Builder
 
     public function __construct(
         \Magento\Framework\App\ResourceConnection $resource
-    ) {
+    )
+    {
         $this->resource = $resource;
         $this->conn = $resource->getConnection();
     }
@@ -36,28 +38,8 @@ abstract class Builder
         return $result;
     }
 
-    /**
-     * Will throw an error if is not overridden.
-     *
-     * @inheritdoc
-     */
-    public function getCountQuery(\Praxigento\Core\Repo\Query\IBuilder $qbuild = null)
-    {
-        throw new \Exception('Implement if you need this query.');
-    }
-
     public function getResource()
     {
         return $this->resource;
-    }
-
-    /**
-     * Will throw an error if is not overridden.
-     *
-     * @inheritdoc
-     */
-    public function getSelectQuery(\Praxigento\Core\Repo\Query\IBuilder $qbuild = null)
-    {
-        throw new \Exception('Implement if you need this query.');
     }
 }
