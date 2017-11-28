b7b6b50983745706b4258c61169fc7645655afd7
Alex Gusev <alex@flancer64.com>
2017-11-28 12:23:22 +0200

    MOBI-999 Fix circular dependency after Asset Transfer implementation

--- src/Api/Service/Customer/Get/Request.php
+++ src/Api/Service/Customer/Get/Request.php
@@ -8,5 +8,22 @@ namespace Praxigento\Core\Api\Service\Customer\Get;
 class Request
     extends \Praxigento\Core\Data
 {
+    const CUSTOMER_ID = 'customerId';
 
+    /**
+     * @return int
+     */
+    public function getCustomerId()
+    {
+        $result = parent::get(self::CUSTOMER_ID);
+        return $result;
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setCustomerId($data)
+    {
+        parent::set(self::CUSTOMER_ID, $data);
+    }
 }
