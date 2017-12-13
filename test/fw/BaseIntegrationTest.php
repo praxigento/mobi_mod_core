7f8869934bf51f6bfcd6d1517bbfb9b1bf6a391f
Alex Gusev <alex@flancer64.com>
2017-12-13 16:28:41 +0200

    MOBI-1027 Replace old "\Praxigento\Accounting\Service\IAccount" with new one

--- test/fw/BaseIntegrationTest.php
+++ test/fw/BaseIntegrationTest.php
@@ -95,8 +95,6 @@ abstract class BaseIntegrationTest
     {
         /** @var  $obj \Praxigento\Core\App\ICached */
         // old services
-        $obj = $this->_manObj->get(\Praxigento\Accounting\Service\IAccount::class);
-        $obj->cacheReset();
         $obj = $this->_manObj->get(\Praxigento\Pv\Service\ISale::class);
         $obj->cacheReset();
         $obj = $this->_manObj->get(\Praxigento\Pv\Service\ITransfer::class);
