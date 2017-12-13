697062403b065a71625080183cda7fb663102c4c
Alex Gusev <alex@flancer64.com>
2017-12-13 20:23:28 +0200

    MOBI-1020 Restructure API requests/responses

---
+++ src/Api/Web/Customer/Get/ByIdInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Core\Api\Web\Customer\Get;
+
+/**
+ * Get customer data by ID.
+ */
+interface ByIdInterface
+{
+    /**
+     * @param \Praxigento\Core\Api\Web\Customer\Get\ById\Request $request
+     * @return \Praxigento\Core\Api\Web\Customer\Get\ById\Response
+     *
+     * Magento 2 WebAPI requires full names in documentation (aliases are not allowed).
+     */
+    public function exec($request);
+}
