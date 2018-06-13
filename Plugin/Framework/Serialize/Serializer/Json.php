b0ffd855d06991d88c39a1e009dab70d42afc8be
Alex Gusev <alex@flancer64.com>
2018-06-13 16:48:59 +0300

    MOBI-1361 "Unable to serialize value" in blog post

---
+++ Plugin/Framework/Serialize/Serializer/Json.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Praxigento\Core\Plugin\Framework\Serialize\Serializer;
+
+/**
+ * MOBI-1358: Check UTF-8 chars in the response content to prevent "Unable to serialize value" error on Cyrillic.
+ */
+class Json
+{
+
+    public function beforeSerialize($subject, $data)
+    {
+        /* see \Magento\Framework\App\PageCache\Kernel::getPreparedData */
+        if (isset($data['content']) && is_string($data['content'])) {
+            $data['content'] = mb_convert_encoding($data['content'], 'UTF-8', 'UTF-8');
+        }
+        return [$data];
+    }
+}
