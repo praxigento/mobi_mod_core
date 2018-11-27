49ca9b44a4c26efbe4e028c42b10fc6ce877d268
Alex Gusev <alex@flancer64.com>
2018-11-27 16:39:41 +0200

    SAN-424 Update price renderer for adminhtml grids

---
+++ view/adminhtml/web/js/grid/columns/number.js
@@ -0,0 +1,47 @@
+/**
+ * UI component to render cell data as a number.
+ *
+ * Based on "./magento/module-ui/view/base/web/js/grid/columns/column.js"
+ */
+define([
+    "Magento_Ui/js/grid/columns/column"
+], function (Column) {
+    "use strict";
+    /**
+     * Formatting function.
+     * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
+     */
+    const language = 'en-US';
+    const fraction = 2;
+    const options = {
+        minimumFractionDigits: fraction,
+        maximumFractionDigits: fraction
+    };
+    const formatter = new Intl.NumberFormat(language, options);
+    const fnFormat = function (num) {
+        const value = Number(num);
+        const result = formatter.format(value);
+        return result;
+    }
+
+    /**
+     * Magento UI component based on 'Column':
+     */
+    return Column.extend({
+        defaults: {
+            bodyTmpl: 'Praxigento_Core/grid/cells/number',
+        },
+
+        /**
+         * Override parent function to format numeric value.
+         *
+         * @param {Object} row
+         * @returns {String}
+         */
+        getLabel: function (row) {
+            const num = Column.prototype.getLabel.apply(this, [row]);
+            const result = fnFormat(num);
+            return result;
+        }
+    });
+});
