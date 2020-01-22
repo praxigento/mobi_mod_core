4485124aa9b3bab8a8764686ce83c5c9d24e4b1a
Alex Gusev <alex@flancer64.com>
2020-01-22 18:37:06 +0200

    SAN-755 Create downgrade log in adminhtml

--- App/Ui/DataProvider/Grid/Base.php
+++ App/Ui/DataProvider/Grid/Base.php
@@ -49,14 +49,14 @@ class Base
             $data[static::UIC_CONFIG][static::UIC_UPDATE_URL] = $val;
         }
         /* these parameters are not used in overwritten Data Provider */
-        $daorting = $manObj->get(\Magento\Framework\Api\Search\ReportingInterface::class);
+        $reporting = $manObj->get(\Magento\Framework\Api\Search\ReportingInterface::class);
         $filterBuilder = $manObj->get(\Magento\Framework\Api\FilterBuilder::class);
         /* init parent */
         parent::__construct(
             $name,
             $primaryFieldName,
             $requestFieldName,
-            $daorting, // $daorting is not used in overwritten class
+            $reporting, // $reporting is not used in overwritten class
             $searchCriteriaBuilder,
             $request,
             $filterBuilder, // $filterBuilder is not used in overwritten class
